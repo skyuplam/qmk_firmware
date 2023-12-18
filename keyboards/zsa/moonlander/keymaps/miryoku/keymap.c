88050c2144c48fb421f36a814692560abb286664
Terrence Lam <terrence@tlam.dev>
2023-12-18 10:52:46 +0100

    Adjust QUICK_TAP_TERM timing

--- keyboards/zsa/moonlander/keymaps/miryoku/keymap.c
+++ keyboards/zsa/moonlander/keymaps/miryoku/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO,             KC_NO,        KC_NO,            KC_NO,               KC_NO, KC_NO,             KC_NO, KC_NO,            KC_NO,            KC_NO,        KC_NO,          KC_NO,                KC_NO,
         KC_NO, KC_Q,              KC_W,         KC_E,             KC_R,                KC_T,  KC_NO,             KC_NO, KC_Y,             KC_U,             KC_I,         KC_O,           KC_P,                 KC_NO,
         KC_NO, LGUI_T(KC_A),      LALT_T(KC_S), LCTL_T(KC_D),     LSFT_T(KC_F),        KC_G,  KC_NO,             KC_NO, KC_H,             LSFT_T(KC_J),     LCTL_T(KC_K), LALT_T(KC_L),   LGUI_T(KC_SCLN),      KC_NO,
-        KC_NO, LT(U_BUTTON,KC_Z), ALGR_T(KC_X), KC_C,             KC_V,                KC_B,        KC_N,             KC_M,             KC_COMM,      ALGR_T(KC_DOT), LT(U_BUTTON,KC_SLSH), KC_NO,
+        KC_NO, LT(U_BUTTON,KC_Z), ALGR_T(KC_X), KC_C,             KC_V,                KC_B,                            KC_N,             KC_M,             KC_COMM,      ALGR_T(KC_DOT), LT(U_BUTTON,KC_SLSH), KC_NO,
         KC_NO, KC_NO,             KC_NO,        KC_NO,            KC_NO,               KC_NO,                           KC_NO,            KC_NO,            KC_NO,        KC_NO,          KC_NO,                KC_NO,
                                                 LT(U_NAV,KC_SPC), LT(U_MOUSE,KC_BSPC), KC_NO,                           KC_NO,            LT(U_SYM,KC_TAB), LT(U_NUM,KC_ENT)
     ),
